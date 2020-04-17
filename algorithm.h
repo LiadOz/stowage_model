74a01991ced87d7c89ca71a4302e1093feaf7af1
Liad Oz <liadozil@gmail.com>
2020-04-17 09:13:04 +0300

    finished brute algorithm

--- algorithm.h
+++ algorithm.h
@@ -3,17 +3,24 @@
 
 #include <string>
 #include <vector>
+#include <unordered_set>
+#include "container.h"
 #include "ship.h"
+
 using std::string;
+using std::unordered_set;
 
 class Algorithm {
 protected:
     Ship s;
     vector<string> routes;
+    unordered_set<Container*> outside;
+    void unloadAll(string port);
 public:
-    Algorithm ();
+    Algorithm (){};
     void readShipPlan(const string& full_path_and_file_name){ s = Ship(full_path_and_file_name); }
     void readShipRoute(const string& full_path_and_file_name);
+    void finalDestination();
     virtual void getInstructionsForCargo(
             const string& input_full_path_and_file_name,
             const string& output_full_path_and_file_name){};
@@ -21,8 +28,14 @@ public:
 
 // the brute algorithm moves every container off the ship
 // then loads them back on
-class BruteAlgroithm : protected Algorithm {
+class BruteAlgroithm : public Algorithm {
+private:
+    // insertes container to the next free location
+    // return false if there is no space left
+    size_t next_x, next_y;
+    bool insertNextFree(Container* c);
 public:
+    BruteAlgroithm (){};
     virtual void getInstructionsForCargo(
             const string& input_full_path_and_file_name,
             const string& output_full_path_and_file_name);
--- main.cpp
+++ main.cpp
@@ -1,28 +1,14 @@
-#include <iostream>
-#include <string>
-#include "container.h"
-#include "ship.h"
+#include "algorithm.h"
 #include "util.h"
-
-using std::string;
-using std::vector;
 
 int main() {
     Logger& l = Logger::Instance();
-    l.setLogType("creating conatiners");
-    Container* c1 = new Container(10, "a", "CSQU3054383");
-    Container* c2 = new Container(10, "a", "CSQU305438s");
-    string a = "example/sample.plan";
-    Ship s(a);
-    l.setLogType("inserting Containers");
-    s.insertContainer(0, 0, c1);
-    s.insertContainer(0, 0, c2);
-    s.removeContainer(0, 0);
-    s.removeContainer(0, 0);
-    s.removeContainer(0, 0);
-    s.removeContainer(0, 0);
-    s.removeContainer(0, 0);
-    s.removeContainer(0, 0);
-    s.removeContainer(0, 0);
-
+    l.setLogType("init");
+    Algorithm a = BruteAlgroithm();
+    a.readShipPlan("./example/sample.plan");
+    a.readShipRoute("./example/ports");
+    l.setLogType("alg1");
+    a.getInstructionsForCargo("./example/AERKT_34.cargo_data", "out1");
+    l.setLogType("alg2");
+    a.getInstructionsForCargo("./example/JPSNR_3.cargo_data", "out2");
 }
