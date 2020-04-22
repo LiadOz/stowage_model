c22f9b5ce07de70174f5f59b5ea66d8ee9d3cace
Liad Oz <liadozil@gmail.com>
2020-04-22 10:43:27 +0300

    added comments

--- algorithm.h
+++ algorithm.h
@@ -21,6 +21,8 @@ protected:
     // return false if there is no space left
     size_t next_x, next_y;
     bool insertNextFree(Container* c);
+    // this function is called inside getInstructionsForCargo and detailes
+    // the operation each algorithm does polymophically
     virtual void getPortInstructions(const string& port,
             const string& input_path){(void)port;(void)input_path;};
 public:
@@ -29,7 +31,7 @@ public:
     void readShipPlan(const string& full_path_and_file_name);
     void readShipRoute(const string& full_path_and_file_name);
     void finalDestination();
-    virtual void getInstructionsForCargo(
+    void getInstructionsForCargo(
             const string& input_full_path_and_file_name,
             const string& output_full_path_and_file_name);
 };
