ca7d450d874a542ed486cc43add7ea41e7af9350
Liad Oz <liadozil@gmail.com>
2020-05-05 12:43:35 +0300

    fixed file format

---
+++ Parser.cpp
@@ -0,0 +1,58 @@
+#include "parser.h"
+#include <iostream>
+#include <sstream>
+using std::stringstream;
+
+Parser::Parser (const string& filePath){
+    LoadFile(filePath);
+}
+void Parser::LoadFile(const string& filePath){
+    readFile.open(filePath);
+    if(!readFile.good()) throw std::runtime_error("invalid file");
+    SkipComments();
+}
+
+bool IsComment(const string& line) {
+
+    // ignoring spaces
+	unsigned index = 0;
+	while (index < line.length()) {
+		if (isspace(line[index]))
+			index++;
+		else 
+			break;
+	}
+
+	if (index == line.length()) {
+		return true;
+	}
+
+	return line[index] == FILE_COMMENT_LINE_CHAR;
+}
+
+void Parser::SkipComments(){
+    string line;
+    int curr;
+    do {
+        curr = readFile.tellg();
+        if(!getline(readFile, line)){
+            moreLines = false;
+            return;
+        }
+    } while (IsComment(line));
+    readFile.seekg(curr);
+
+}
+
+Parser& Parser::operator>>(vector<string>& data){
+    string line;
+    getline(readFile, line);
+    stringstream ss(line);
+    while(ss.good()){
+        string word;
+        getline(ss, word, ',');
+        data.push_back(word);
+    }
+    SkipComments();
+    return *this;
+}
---
+++ Ship.cpp
@@ -0,0 +1,56 @@
+#include <stdexcept>
+#include "ship.h"
+
+using std::to_string;
+
+int Ship::ReadPlan(const string& file_path) { 
+    int errorStatus = storage.ReadPlan(file_path);
+    decks = storage.GetNumFloors();
+    return errorStatus;
+}
+
+void ShipLogger::LogAction(const string& action){
+    file << action << std::endl;
+    if (logToScreen)
+        std::cout << "(ship logger): " << action << std::endl;
+}
+void Ship::InsertContainer(size_t x, size_t y, Container& c){
+    // TODO: add calculator check
+    storage.PushContainer(x, y, c);
+    logger.LogAction("<L>," + c.GetId() + "," +
+            to_string(GetCoordinateHeight(x, y)) + 
+            "," + to_string(x) + "," + to_string(y)
+            );
+}
+
+Container Ship::RemoveContainer(size_t x, size_t y){
+    // TODO: add calculator check
+    int h = GetCoordinateHeight(x, y);
+    Container c = storage.PopContainer(x, y);
+    logger.LogAction("<U>," + c.GetId() + "," +
+            to_string(h) + "," + to_string(x) + "," + to_string(y)
+            );
+    return c;
+}
+
+void Ship::MoveContainer(size_t x1, size_t y1, size_t x2, size_t y2){
+    // TODO: add calculator check
+    int firstHeight = GetCoordinateHeight(x1, y1);
+    Container c = storage.PopContainer(x1, y1);
+    try {
+        storage.PushContainer(x2, y2, c);
+    }catch(std::out_of_range& e) {
+        storage.PushContainer(x1, y1, c);
+        throw e;
+    }
+    logger.LogAction("<M>," + c.GetId() + "," +
+            to_string(firstHeight) + "," + 
+            to_string(x1) + "," + to_string(y1) +
+            to_string(GetCoordinateHeight(x2, y2)) + "," + 
+            to_string(x2) + "," + to_string(y2) 
+            );
+}
+
+void Ship::RejectContainer(Container& c){
+    logger.LogAction("<R>," + c.GetId());
+}
