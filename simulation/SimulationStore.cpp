8ada87313ee0cb359c3e9f8647ebb5bc73963e55
Liad Oz <liadozil@gmail.com>
2020-06-18 21:47:08 +0300

    added do nothing algorithms

--- simulation/SimulationStore.cpp
+++ simulation/SimulationStore.cpp
@@ -6,33 +6,19 @@
 #include "Port.h"
 
 vector<Port> SimulationStore::getRouteFromStore(const string& routePath) {
-    if (!isValidFile(routePath)) {
-        throw FatalError(routePath + " is invalid.");
-    }
-
     auto routeIterator = this->routeStore.find(routePath);
     if (routeIterator != this->routeStore.end()) {
         return routeIterator->second;
     }
 
     //not stored already, read from file and store it
-    try {
-        auto newRoute = createShipRoute(routePath);
-        this->routeStore.insert(std::pair<string, vector<Port>>(routePath, newRoute));
-        std::cout << "reading " + routePath + "for first time (?)" << std::endl;
-        return newRoute;
-    }
+    auto newRoute = createShipRoute(routePath);
+    this->routeStore.insert(std::pair<string, vector<Port>>(routePath, newRoute));
+    return newRoute;
 
-    catch (FatalError& ferror) {
-        invalidateFiles.insert(routePath);
-        throw ferror;
-    }
 }
 
 Ship SimulationStore::getShipFromStore(const string& shipPlanPath) {
-    if (!isValidFile(shipPlanPath)) {
-        throw FatalError(shipPlanPath + " is invalid.");
-    }
 
     auto shipIterator = this->shipPlanStore.find(shipPlanPath);
     if (shipIterator != this->shipPlanStore.end()) {
@@ -40,26 +26,16 @@ Ship SimulationStore::getShipFromStore(const string& shipPlanPath) {
     }
 
     //not stored already, read from file and store it
-    try {
-        Ship ship;
-        ship.readPlan(shipPlanPath);
-        vector<Ship> vs;
-        std::pair<string, vector<Ship>> pairr;
-        this->shipPlanStore.insert(std::pair<string, Ship>(shipPlanPath, ship));
-        std::cout << "\t reading " + shipPlanPath + "for first time (?)" << std::endl;
-        return ship;
-    }
+    Ship ship;
+    ship.readPlan(shipPlanPath);
+    vector<Ship> vs;
+    std::pair<string, vector<Ship>> pairr;
+    this->shipPlanStore.insert(std::pair<string, Ship>(shipPlanPath, ship));
+    return ship;
 
-    catch (FatalError& ferror) {
-        invalidateFiles.insert(shipPlanPath);
-        throw ferror;
-    }
 }
 
 bool SimulationStore::isValidFile(const string& path) {
-    
-        auto invalidFileIterator = invalidateFiles.find(path);
-        return (invalidFileIterator == invalidateFiles.end());
-    
-    return true;
+    auto invalidFileIterator = invalidateFiles.find(path);
+    return (invalidFileIterator == invalidateFiles.end());
 }
